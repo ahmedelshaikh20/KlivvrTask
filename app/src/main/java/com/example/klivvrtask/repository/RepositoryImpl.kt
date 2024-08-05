68f6520e590d058cebfaf0c6d8c86e85990ec1f3
Ahmed Salah <ahmedelshaikh51@gmail.com>
2024-08-05 21:15:45 +0300

    Moving the logic of reading the json to file service

--- app/src/main/java/com/example/klivvrtask/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/klivvrtask/repository/RepositoryImpl.kt
@@ -1,50 +1,40 @@
 package com.example.klivvrtask.repository
 
 import android.content.Context
-import com.example.klivvrtask.R
 import com.example.klivvrtask.domain.model.City
 import com.example.klivvrtask.domain.repository.Repository
+import com.example.klivvrtask.util.readJson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
-import kotlin.coroutines.CoroutineContext
 
 object RepositoryImpl : Repository() {
 
   private val nameMap = HashMap<String, MutableList<City>>()
   private val countryMap = HashMap<String, MutableList<City>>()
   private var entries = emptyList<City>()
-  override suspend fun fetchAndMapFile(context: Context): List<City> = withContext(Dispatchers.IO) {
+  override suspend fun loadAndMapCities(context: Context): List<City> = withContext(Dispatchers.IO) {
 
-    val jsonString = context.resources.openRawResource(R.raw.cities).bufferedReader().readText()
-    val json = Json { ignoreUnknownKeys = true }
-    entries = json.decodeFromString<List<City>>(jsonString).sortedBy { it.name }
+    entries = readJson(context)
 
-    var count = 0;
-    var mapind = 0;
     for (entry in entries) {
       for (i in 1..entry.name.length) {
         val prefix = entry.name.substring(0, i)
         nameMap.computeIfAbsent(prefix) { mutableListOf() }.add(entry)
-        mapind++;
       }
       for (i in 1..entry.country.length) {
         val prefix = entry.country.substring(0, i)
         countryMap.computeIfAbsent(prefix) { mutableListOf() }.add(entry)
       }
-      count++;
     }
-    println(mapind)
     return@withContext entries
   }
 
   override fun filterWithQuery(query: String): List<City> {
-    if (query == "") {
+    if (query.isEmpty()) {
       return entries
     }
     val resFromName = nameMap.get(query) ?: emptyList()
-
-    return (resFromName ).sortedBy { it.name }
+    return (resFromName).sortedBy { it.name }
 
   }
 
--- app/src/main/java/com/example/klivvrtask/util/FileService.kt
+++ app/src/main/java/com/example/klivvrtask/util/FileService.kt
@@ -1,4 +1,13 @@
 package com.example.klivvrtask.util
 
-class FileService {
+import android.content.Context
+import com.example.klivvrtask.R
+import com.example.klivvrtask.domain.model.City
+import kotlinx.serialization.json.Json
+
+fun readJson(context: Context ): List<City>{
+  val jsonString = context.resources.openRawResource(R.raw.cities).bufferedReader().readText()
+  val json = Json { ignoreUnknownKeys = true }
+  return json.decodeFromString<List<City>>(jsonString).sortedBy { it.name }
+
 }
