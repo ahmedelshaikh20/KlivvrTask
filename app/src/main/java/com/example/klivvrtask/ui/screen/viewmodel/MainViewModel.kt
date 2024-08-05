38fe8c272f3ab27c8d3b312d785bea0ec8873a5c
Ahmed Salah <ahmedelshaikh51@gmail.com>
2024-08-05 20:08:35 +0300

    Renaming File

--- app/src/main/java/com/example/klivvrtask/ui/screen/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/klivvrtask/ui/screen/viewmodel/MainViewModel.kt
@@ -6,21 +6,33 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.klivvrtask.ui.screen.MainScreenEvents
 import com.example.klivvrtask.ui.screen.MainScreenState
-import com.example.klivvrtask.usecase.FetchAndMapFile
+import com.example.klivvrtask.usecase.FetchAndMapFileUseCase
+import com.example.klivvrtask.usecase.FilterCitiesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
-class MainViewModel @Inject constructor(val fetchAndMapFile: FetchAndMapFile) : ViewModel() {
+class MainViewModel @Inject constructor(val fetchAndMapFileUseCase: FetchAndMapFileUseCase, val filterCitiesUseCase: FilterCitiesUseCase) : ViewModel() {
   var state by mutableStateOf(MainScreenState())
   fun fetchFile(context: Context) {
     viewModelScope.launch {
-      state = state.copy(currentCities = fetchAndMapFile(context))
+      state = state.copy(currentCities = fetchAndMapFileUseCase(context), isLoaded = true)
     }
   }
 
+
+  fun onEvent(event: MainScreenEvents) {
+    when (event) {
+      is MainScreenEvents.OnQueryChange -> {
+        state = state.copy(query = event.query , currentCities = filterCitiesUseCase(event.query) )
+      }
+
+    }
+
+  }
 
 }
--- app/src/main/java/com/example/klivvrtask/usecase/FilterCitiesUseCase.kt
+++ app/src/main/java/com/example/klivvrtask/usecase/FilterCitiesUseCase.kt
@@ -1,3 +1,12 @@
 package com.example.klivvrtask.usecase
 
-class FilterCitiesUseCase 
+import android.content.Context
+import com.example.klivvrtask.domain.repository.Repository
+import javax.inject.Inject
+
+class FilterCitiesUseCase @Inject constructor(val repository: Repository) {
+
+  operator fun invoke(query: String) = repository.filterWithQuery(query)
+
+
+}
